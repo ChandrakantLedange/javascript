979349e97923f196c036b91221d15e73fae609d0
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2024-06-27 08:36:27 +0530

    PTC: js day1

---
+++ Day1/day1.js
@@ -0,0 +1,20 @@
+//singleline comment
+
+/* multiline 
+comment*/
+
+//Variable
+let string = "javascript";
+let married = false;
+let myClass = 12;
+let age;
+
+console.log(string); //javascript
+console.log(married); //false
+console.log(myClass); //12
+console.log(age); //undefined
+
+console.log(typeof string); //string
+console.log(typeof married); //boolean
+console.log(typeof myClass); // number
+console.log(typeof age); // undefined
